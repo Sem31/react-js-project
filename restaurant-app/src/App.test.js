23bba8b9d2a79711fa5b11cdcd6bcb4e28661f6f
Sem31 <31kamleshprajapat@gmail.com>
2020-07-11 20:07:45 +0530

    write two testcases in app.test.js file

--- restaurant-app/src/App.test.js
+++ restaurant-app/src/App.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+test('object assignment', () => {
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
+
+test('API object assignment', () => {
+  const list=null;
+  fetch("http://localhost:3000/restaurant").then((response) => {
+            response.json().then((result) => {
+                this.setState({list:result})
+      })
+    })
+  const data = list;
+  expect(data).toEqual(list);
 });
